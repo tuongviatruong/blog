758c7b563eb013672535924f4dc1dd236bf5f9f9
vagrant <vagrant@vagrant.vm>
2018-11-07 00:44:09 +0000

    model.py

---
+++ model.py
@@ -0,0 +1,62 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+# class User(db.Model):
+#     """User of website"""
+#     __tablename__ = "users"
+
+#     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     fname = db.Column(db.String(64))
+#     lname = db.Column(db.String(64))
+#     email = db.Column(db.String(64))
+#     password = db.Column(db.String(64))
+
+#     def __repr__(self):
+#         """Provide helpful representation when printed"""
+#         return "<User user_id={} fname={} lname={}>".format(self.user_id,
+#                                                         self.fname, self.lname)
+
+class Blog(db.Model):
+    """Blogs users uplaod"""
+    __tablename__ = "blogs"
+
+    blog_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(100))
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    # user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+
+    #Define relationship to user
+    # user = db.relationship("User", backref="blogs")
+
+
+
+    def __repr__(self):
+        """Provide helpful representation when printed"""
+        return "<Blog blog_id={} title={}>".format(self.blog_id, self.title)
+
+
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app, db_uri='postgresql:///blog'):
+    """Connect the database to Flask app."""
+
+    # Configure to use PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to DB."
--- server.py
+++ server.py
@@ -1,17 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime
+from jinja2 import StrictUndefined
+from model import connect_to_db, db, Blog
 
 app = Flask(__name__)
 app.secret_key = "SECRETSECRETSECRET"
 app.jinja_env.undefined = StrictUndefined
+
+posts = [
+    {
+        'title': 'Blog Post 1',
+        'description': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'title': 'Blog Post 2',
+        'description': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 
 @app.route('/')
 def homepage():
     """Show homepage"""
 
-    return render_template("homepage.html")
+    return render_template("homepage.html", posts=posts)
+
 
 
 
